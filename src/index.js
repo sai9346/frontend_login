496f6d83b6d560a7828181ba384a813dd662a48e
KARNATAKA SAIKUMAR <81470320+sai9346@users.noreply.github.com>
2024-09-08 23:36:27 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Importing your main App component
-import './index.css'; // Importing a basic stylesheet
+import App from './App';
+import './index.css'; 
 
-// Creating a root and rendering the App component inside the 'root' div in index.html
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
